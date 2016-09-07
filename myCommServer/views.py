890a4d04a0bbd6cd1f67651fdc566fb6889b3a4a
John Mac <johngrantuk@googlemail.com>
2016-09-07 19:48:49 +0100

    Adding message to page.

--- myCommServer/views.py
+++ myCommServer/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import UserMsg
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def messages(request):
@@ -12,11 +14,19 @@ def messages(request):
 def incomingMessage(request):
     print("\nINCOMINGMESSAGE\n")
     if request.method == 'POST':
+        u = User.objects.get(username='scotsat')
+        postDict = request.POST
+        data = postDict.get("data")
+        userMessage = UserMsg(user=u, message=data, destinationId="jack", receivedDate=timezone.now())
+        userMessage.save()
+
+        """
         print("\nPOST:\n")
         print(request.POST)
         print("\nBody:\n")
         print(request.body)
         print("\nParams:\n")
         print(request.content_params)
+        """
 
     return HttpResponse(status=200)
