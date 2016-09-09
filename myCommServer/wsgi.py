40b25bf7b297525c09309caa08eba19ad4cf5bd2
John Mac <johngrantuk@googlemail.com>
2016-09-09 14:53:27 +0100

    Whitenoise config.

--- myCommServer/wsgi.py
+++ myCommServer/wsgi.py
@@ -11,9 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myCommServer.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
